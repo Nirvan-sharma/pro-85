3c1b3670ca9e81e8d2faf7c533d30b633ecc8fc8
Nirvan-sharma <78906217+Nirvan-sharma@users.noreply.github.com>
2021-05-01 15:04:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+car_width=120;
+car_height=90;
+
+car1_image="car1.png";
+background_image="track.jpg";
+car_x=10;
+car_y=10;
+
+function add(){
+    background_imgTag=new Image();
+    background_imgTag.onload=uploadBackground;
+    background_imgTag.src=background_image;
+
+    
+    car_imgTag=new Image();
+    car_imgTag.onload=uploadcar;
+    car_imgTag.src=car1_image;
+}
+
+function uploadBackground(){
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadcar(){
+    ctx.drawImage(car_imgTag, car_x, car_y,car_width, car_height);
+}
+
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e){
+    keypressed=e.keyCode;
+
+    if(keypressed=="38"){
+        up();
+    }
+
+    if(keypressed=="40"){
+        down();
+    }
+
+    if(keypressed=="37"){
+        left();
+    }
+
+    if(keypressed=="39"){
+        right();
+    }
+}
+
+function up() { 
+    if(car_y >=0) { 
+        car_y = car_y - 10;
+         console.log("When up arrow is pressed, x = " + car_x + " | y = " +car_y);
+          uploadBackground(); 
+          uploadcar();
+         }
+}
+
+function down() { 
+    if(car_y <=500) { 
+        car_y = car_y+ 10;
+         console.log("When down arrow is pressed, x = " + car_x + " | y = " +car_y);
+          uploadBackground(); 
+          uploadcar();
+         }
+}
+function left() { 
+    if(car_x >=0) { 
+        car_x = car_x - 10;
+         console.log("When left is pressed, x = " + car_x + " | y = " +car_y);
+          uploadBackground(); 
+          uploadcar();
+         }
+}
+function right() { 
+    if(car_x <=700) { 
+        car_x = car_x + 10;
+         console.log("When right arrow is pressed, x = " +car_x + " | y = " +car_y);
+          uploadBackground(); 
+          uploadcar();
+         }
+}
